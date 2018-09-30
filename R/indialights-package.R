f2c85b5239ccf1cbb38c621c554840f59bc75732
boB Rudis <bob@rud.is>
2018-09-30 10:40:03 -0400

    R package repo initialization complete

---
+++ R/indialights-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/indialights>
+#' - BugReports: <https://gitlab.com/hrbrmstr/indialights/issues>
+#' 
+#' @md
+#' @name indialights
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
